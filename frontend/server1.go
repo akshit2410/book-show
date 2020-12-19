990332e5ccfbed7126d966b5e1242ca76e027a5f
Akshit <42813910+akshit2410@users.noreply.github.com>
2020-12-20 01:03:14 +0530

    Update server1.go

--- frontend/server1.go
+++ frontend/server1.go
@@ -1,17 +1,17 @@
 package main
 import (
-  //"fmt"
+  "fmt"
   "github.com/gin-gonic/contrib/static"
   "os"
   "github.com/gin-gonic/gin"
 )
 func main()  {
   port := os.Getenv("PORT")
-
  if port == "" {
    port = "2404"
  }
   server :=gin.Default()
   server.Use(static.Serve("/", static.LocalFile("./templates", true)))
+  fmt.Println("hey its working",port)
 server.Run(":"+port)
 }
