ec098eab17e0c6f7bec60a090ede1f15cd9ef402
Akshit <42813910+akshit2410@users.noreply.github.com>
2020-12-31 19:21:15 +0530

    Update conn.go

--- backend/databaseconn/conn.go
+++ backend/databaseconn/conn.go
@@ -9,7 +9,7 @@ import(
 )
 //connecting to DB
 func Connect() *sql.DB {
-  db,err := sql.Open("mysql","root:akshit@tcp(db)/showtime")
+  db,err := sql.Open("mysql","root@tcp(127.0.0.1)/showtime")
   if err!=nil {
       fmt.Println(err.Error())
   }
@@ -80,35 +80,69 @@ func Create(reg R.Register) int {
 }
 }
 //check if user present
-func Checkuserpresence(log R.Login) (string)  {
+func Checkuserpresence(log R.Login) (string,int)  {
   var (
     username string
     password string
+    level int
   )
   db := Connect()
-  querycheck := "SELECT username,password FROM user WHERE username = ?"
+  querycheck := "SELECT username,password,level FROM user WHERE username = ?"
   rows,err :=db.Query(querycheck,log.USER)
   PanicError(err)
   defer rows.Close()
   if rows.Next() {
-    err := rows.Scan(&username,&password)
+    err := rows.Scan(&username,&password,&level)
     PanicError(err)
-    return password
+    return password,level
   }
-  return ""
+  return "",0
 }
 //logging in a user
 func CheckCredentials(log R.Login) (int,string) {
-  hashpass := Checkuserpresence(log)
-
+  hashpass,level := Checkuserpresence(log)
+  fmt.Println(level)
   if hashpass != "" {
-    if ComparePasswords(hashpass,log.PASSWORD) {
+    if ComparePasswords(hashpass,log.PASSWORD) && level==0 {
       return 200,log.USER
-    }else{
+    }else if ComparePasswords(hashpass,log.PASSWORD) && level==1 {
+      return 500,log.USER
+      }else{
       return 201,""
     }
   }else {
     return 202,""
   }
 
+}
+
+
+func Addmovie(info R.Addmovie) int  {
+  db :=  Connect()
+  fmt.Println(info)
+  queryinsert := "INSERT INTO movie(mname,mdesc,mdirector,mduration,image) VALUES(?,?,?,?,?)"
+  insert,err :=  db.Prepare(queryinsert)
+  PanicError(err)
+  insert.Exec(info.MNAME,info.MDESC,info.MDIRECTOR,info.MDURATION,info.IMAGE)
+  defer db.Close()
+  return 200
+
+}
+
+func Addtime(info R.Addtime) int  {
+  db :=  Connect()
+  querycheck := "SELECT mname FROM movie WHERE mname = ?"
+  rows,err :=db.Query(querycheck,info.MNAME)
+  PanicError(err)
+  defer rows.Close()
+  if rows.Next() {
+    queryinsert := "INSERT INTO timing(mname,start_time,end_time,date,quantity,price) VALUES(?,?,?,?,?,?)"
+    insert,err :=  db.Prepare(queryinsert)
+    PanicError(err)
+    insert.Exec(info.MNAME,info.START_TIME,info.END_TIME,info.DATE,info.QUANTITY,info.PRICE)
+    defer db.Close()
+    return 200
+  }
+
+return 400
 }
